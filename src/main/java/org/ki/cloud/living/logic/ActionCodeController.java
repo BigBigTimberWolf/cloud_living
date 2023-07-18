758be37a54eb443d9d595b199b62887dc5f4db7d
xiaohan <hhhhyuan@outlook.com>
2023-07-18 21:44:09 +0800

    11

--- src/main/java/org/ki/cloud/living/logic/ActionCodeController.java
+++ src/main/java/org/ki/cloud/living/logic/ActionCodeController.java
@@ -22,47 +22,37 @@ public class ActionCodeController {
 
     @RequestMapping("/register/{project}/{deviceSignatureCode}")
     public Mono<Result<RegisterResponseModel>> registerDevice(@PathVariable String deviceSignatureCode, @PathVariable String project) {
+        RegisterRequestModel registerRequestModel = new RegisterRequestModel();
+        registerRequestModel.setDeviceSignatureCode(deviceSignatureCode);
+        registerRequestModel.setProject(project);
+        return registerDevice2(registerRequestModel);
 
-        return actionCodeDao.findByDeviceSignatureCode(deviceSignatureCode)
-                .defaultIfEmpty(new TActionCode())
-                .flatMap(a -> {
-                    if (a.getDeviceName()==null){
-                        return actionCodeDao.countByDeviceSignatureCode().flatMap(i->{
-                            if (i!=0){
-                                return actionCodeDao.registerDevice(deviceSignatureCode, project)
-                                        .then(Mono.defer(() -> actionCodeDao.findByDeviceSignatureCode(deviceSignatureCode)));
-                            }
-                            return Mono.empty();
-                        }).switchIfEmpty(Mono.defer(()->Mono.just(a)));
-                    }else {
-                        return actionCodeDao.findByDeviceSignatureCode(deviceSignatureCode);
-                    }
-                })
-                .map(ss -> {
-                    Result<RegisterResponseModel> registerResponseModelResult = new Result<>();
-                    if (ss.getDeviceSignatureCode()==null){
-                        registerResponseModelResult.setCode(400);
-                        registerResponseModelResult.setMessage("项目"+project+"的key不足");
-                        return registerResponseModelResult;
-                    }
-                    return registerResponseModelResult.ok(new RegisterResponseModel(ss));
-                })
-                ;
 
     }
 
     @RequestMapping("/register")
     public Mono<Result<RegisterResponseModel>> registerDevice1(RegisterRequestModel registerRequestModel) {
 
+        return registerDevice2(registerRequestModel);
+
+    }
+
+    private Mono<Result<RegisterResponseModel>> registerDevice2(RegisterRequestModel registerRequestModel) {
         return actionCodeDao.findByDeviceSignatureCode(registerRequestModel.getDeviceSignatureCode())
                 .defaultIfEmpty(new TActionCode())
-                .flatMap(a -> actionCodeDao.countByDeviceSignatureCode().flatMap(i->{
-                    if (i!=0){
-                        return actionCodeDao.registerDevice(registerRequestModel.getDeviceSignatureCode(), registerRequestModel.getProject())
-                                .then(Mono.defer(() -> actionCodeDao.findByDeviceSignatureCode(registerRequestModel.getDeviceSignatureCode())));
+                .flatMap(a -> {
+                    if (a.getDeviceName()==null){
+                        return actionCodeDao.countByDeviceSignatureCode().flatMap(i->{
+                            if (i!=0){
+                                return actionCodeDao.registerDevice(registerRequestModel.getDeviceSignatureCode(), registerRequestModel.getProject())
+                                        .then(Mono.defer(() -> actionCodeDao.findByDeviceSignatureCode(registerRequestModel.getDeviceSignatureCode())));
+                            }
+                            return Mono.empty();
+                        }).switchIfEmpty(Mono.defer(()->Mono.just(a)));
+                    }else {
+                        return actionCodeDao.findByDeviceSignatureCode(registerRequestModel.getDeviceSignatureCode());
                     }
-                    return Mono.empty();
-                }).switchIfEmpty(Mono.defer(()->Mono.just(a))))
+                })
                 .map(ss -> {
                     Result<RegisterResponseModel> registerResponseModelResult = new Result<>();
                     if (ss.getDeviceSignatureCode()==null){
@@ -73,7 +63,6 @@ public class ActionCodeController {
                     return registerResponseModelResult.ok(new RegisterResponseModel(ss));
                 })
                 ;
-
     }
 
     @GetMapping("/all")
